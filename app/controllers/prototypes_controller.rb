23ff1bdd7e5402dcb5f06d3d5ae2e06058801408
yoshihiro-kawamura <73100863+yoshihiro-kawamura@users.noreply.github.com>
2020-11-11 18:42:01 +0900

    訂正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :index, :show, ]
+  before_action :authenticate_user!, only: [:new, :show]
   before_action :move_to_index, only: [:edit]
 
   def index
