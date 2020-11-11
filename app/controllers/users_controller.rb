ce5ff0f377940b7768e5408f6df402e3b0316d3b
yoshihiro-kawamura <73100863+yoshihiro-kawamura@users.noreply.github.com>
2020-11-11 17:04:23 +0900

    ユーザー機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,11 @@
 class UsersController < ApplicationController
 
   def show 
-    @user = User.find(params[:id])
+    user = User.find(params[:id])
+    @users = user.users
+    @profile = user.profile
+    @occupation = user.occupation
+    @position = user.position
     @prototypes = Prototype.all
   end
-
 end
